37bfa51bcbfe36bf115dd7bad988737f0ca07553
unknown <jacobandrey971@gmail.com>
2021-05-20 06:48:02 +1000

    initial commit

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,36 @@
+export const links = [
+  {
+    label: "Swap",
+    icon: "FarmIcon",
+    href: "/swap",
+  },
+  {
+    label: "Farms",
+    icon: "FarmIcon",
+    href: "https://defi.finance/farms",
+  },
+  {
+    label: "Stake",
+    icon: "FarmIcon",
+    href: "https://defi.finance/stake",
+  },
+
+];
+
+export const socials = [
+  {
+    label: "Telegram",
+    icon: "TelegramIcon",
+    href: "https://t.me/woonkly",
+  },
+  {
+    label: "Twitter",
+    icon: "TwitterIcon",
+    href: "https://twitter.com/woonkly",
+  },
+];
+
+export const MENU_HEIGHT = 64;
+export const MENU_ENTRY_HEIGHT = 48;
+export const SIDEBAR_WIDTH_FULL = 240;
+export const SIDEBAR_WIDTH_REDUCED = 56;
