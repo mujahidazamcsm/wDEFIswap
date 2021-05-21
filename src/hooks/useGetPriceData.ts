38811c9b5f40a0bfa3c95ca824873afa12036981
unknown <jacobandrey971@gmail.com>
2021-05-21 19:39:24 +1000

    built

--- src/hooks/useGetPriceData.ts
+++ src/hooks/useGetPriceData.ts
@@ -15,7 +15,8 @@ type ApiResponse = {
  */
 const WDEFI_ADDRESS = '0x291083c8aedfc3cd0384494e1fcdcf2067d28d3e'
 const WOOP_ADDRESS = '0x8b303d5bbfbbf46f1a4d9741e491e06986894e18'
-const api = 'https://api.pancakeswap.info/api/v2/tokens/' + WDEFI_ADDRESS
+const api = 'https://api.pancakeswap.info/api/v2/tokens/' + WDEFI_ADDRESS  // V2
+// const api = 'https://api.pancakeswap.info/api/tokens/' + WDEFI_ADDRESS // V1
 
 const useGetPriceData = () => {
   const [data, setData] = useState<ApiResponse | null>(null)
